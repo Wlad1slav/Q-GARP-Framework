c40764d820e95ad7acf71f1122f9224bff45c8d0
Rasik <vlad.fokin2004.vf@gmail.com>
2026-06-01 23:53:29 +0300

    chore: opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,13 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Quality Growth at a Reasonable Price",
   description: "Quality growth checklist / Чеклист якісного зростання за розумною ціною",
+  openGraph: {
+    title: "Quality Growth at a Reasonable Price",
+    description: "Quality growth checklist / Чеклист якісного зростання за розумною ціною",
+    siteName: "Invest Rate",
+    locale: "uk_UA",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
